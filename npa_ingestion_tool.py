41edb91fc86bfb05d46658fe8c90ad69d88931a5
A-Maras <andrew_maras@sfu.ca>
2021-01-22 13:38:55 -0800

    Code refactoring
    Added way to enter file with links of urls on each line that is parsed.
    
    Also, split up database connection/creation/insertion into separate functions. Added docstring aswell and cleaned up code.
    
    Not sure how to sepatate RSS parsing and SQL database any furthur in main script. Need to spend more time figuring out SQL database.

--- npa_ingestion_tool.py
+++ npa_ingestion_tool.py
@@ -9,19 +9,27 @@ import feedparser
 import re
 import json
 import sqlite3
-from datetime import  datetime
+from datetime import datetime
 import os
 
-def get_archives_rss_urls(url):
-        command_string = "waybackpack --list " + str(url)
-        os.system(command_string)
 
-def parse_rss(url):
+def get_archives_rss_urls(url):
+    command_string = "waybackpack --list " + str(url)
+    os.system(command_string)
+
+
+def parse_rss(url, preview_file):
+    """ parse a RSS feed using feedparser to the gather list of DOIs from the
+                specified RSS feed URL
+            :param url: string of url link
+            :param preview_file: json file to preview RSS parse
+            :return: list of unique DOIs
+            """
     # Parse RSS url w/ feedparser to get consistent format
     rss_feed = feedparser.parse(url)
 
     # Creation of JSON file for easy viewing
-    with open("some_file.json", "w") as f:
+    with open(preview_file, "w") as f:
         json.dump(rss_feed, f)
 
     # New list for found DOIs (will end up with duplicates)
@@ -41,27 +49,46 @@ def parse_rss(url):
     return unique_doi_list
 
 
-def sqlite3_database(doi_list):
-    # Database initialization
-    db = sqlite3.connect('NPA_Ingestion_database.db')
-    cursor = db.cursor()
+def sqlite3_db_initialization(db_file):
+    """ create a database connection to the SQLite database
+            specified by db_file
+        :param db_file: database file
+        :return: Connection object or None
+        """
+    db_connection = sqlite3.connect(db_file)
+    return db_connection
 
-    # Table creation
+
+def sqlite3_table_creation(connection_object):
+    """ create a table from the create_table_sql statement
+        :param connection_object: Connection object
+        :return: Cursor object or None
+        """
+    cursor = connection_object.cursor()
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS DOIs (Doi VARCHAR UNIQUE NOT NULL, created NOT NULL, ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL)")
+        "CREATE TABLE IF NOT EXISTS DOIs (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Doi VARCHAR UNIQUE NOT NULL, created NOT NULL)")
+    return cursor
 
+
+def sqlite3_insertion(doi_input_list, cursor_object, connection_object):
+    """ insert data into the SQLite3 database
+            :param doi_input_list: list of DOI's
+            :param cursor_object: Cursor_object
+            :param connection_object: Connection object
+            :return: Cursor object or None
+            """
     # Data insertion into the database
-    try:
-        with db:
-            for item in doi_list:
-                db.execute("INSERT INTO DOIs(Doi, created) VALUES(?, ?)", (item, datetime.now()))
-    except sqlite3.IntegrityError:
-        print("Warning: Duplicate entry detected!")
-        pass
+    for item in doi_input_list:
+        try:
+            with connection_object:
+                connection_object.execute("INSERT INTO DOIs(Doi, created) VALUES(?, ?)", (item, datetime.now()))
+        except sqlite3.IntegrityError:
+            print("Warning: Duplicate entry detected!")
+            pass
 
     # Query database
-    for row in db.execute("SELECT * from DOIs"):
-        print(row)
+    #for row in connection_object.execute("SELECT * from DOIs"):
+        #print(row)
 
-    db.commit()
-    db.close()
+    #connection_object.commit()
+    #connection_object.close()
