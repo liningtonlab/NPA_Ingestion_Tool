ab0e529760005f63b8daa7fd923a974dea6ed82b
A-Maras <andrew_maras@sfu.ca>
2021-02-03 17:18:40 -0800

    Almost figured out archived rss parsing
    doi_date_locator
    	• Queries db for old doi's and gets the info
    	• Uses date_conversion to convert long date into proper form to be used
    		○ https://regexr.com/5lmkb
    
    date_check
    With date, get the files with same date from list of files.
    	• EllipticalGrayRuntimeenvironment
    
    rss_parse_archive
    With file, scan for correct DOI and check if title/abstract
    	• WonderfulHarmlessDiscussions
    	• HAVE to fix abstract parsing as it is not right, stated that abstract not present, but just located in different place, need to look at more RSS fees for more info
    
    Next step is to figure out how to UPDATE the found title/abstract into the database.
    	• https://www.tutlane.com/tutorial/sqlite/sqlite-update-statement

--- npa_ingestion_tool.py
+++ npa_ingestion_tool.py
@@ -137,7 +137,7 @@ def sqlite3_table_creation(connection_object):
         """
     cursor = connection_object.cursor()
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS DOIs (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Doi VARCHAR UNIQUE NOT NULL, Title VARCHAR, Abstract VARCHAR, Created NOT NULL)")
+        "CREATE TABLE IF NOT EXISTS DOIs (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,Doi VARCHAR UNIQUE NOT NULL, Title VARCHAR, Abstract VARCHAR, Created NOT NULL)")
     return cursor
 
 
@@ -193,3 +193,85 @@ def sqlite3_insertion_only_doi(connection_object, doi):
     except sqlite3.IntegrityError:
         print("Warning: Duplicate entry detected!")
         pass
+
+
+def date_conversion(raw_date):
+    """ converts a raw date with extra characters, takes needed part(y-m-d)
+                    :param raw_date: raw form of date with other unnecessary characters
+                    :return: final date, in a clean form ready to search files names with
+                    """
+    date_match = re.search("([0-9]{4}\-{1}[0-9]{2}\-[0-9]{2})", raw_date)
+    return date_match.group(0)
+
+
+def doi_date_locator(cursor_statement):
+    """ Queries database for 3 week old DOIs for id, doi and the date
+                        :param cursor_statement: cursor object
+                        :return: list of dictionaries that contains id, doi and date
+                        """
+    select_statement = cursor_statement.execute(
+        "SELECT * FROM DOIs WHERE Abstract IS NULL and Created <= date('now', '-21 day')")
+
+    date_hits = [{"id": row[0], "doi": row[1], "date": date_conversion(row[4])} for row in select_statement]
+    return date_hits
+
+
+def date_check(file_list, date_time):
+    """ checks a list of files for the dates in question
+                :param file_list: list of raw xml files stored as json
+                :param date_time: Date of the rss feed archival, insertion date
+                :return: list of filename matches, to be used with rss_parse_archive as function to use within loop
+                """
+
+    date_match = re.search("([0-9]{4}\-{1}[0-9]{2}\-[0-9]{2})", date_time)
+    match_list = [item for item in file_list if date_match.group(0) == re.search("([0-9]{4}\-{1}[0-9]{2}\-[0-9]{2})", item).group(0)]
+    '''for item in file_list:
+        file_match = re.search("([0-9]{4}\-{1}[0-9]{2}\-[0-9]{2})", item)
+        if date_match.group(0) == file_match.group(0):
+            match_list.append(item)'''
+    return match_list
+
+
+def rss_parse_archive(file, id, doi):
+    # TODO: Fix abstract parsing, but mainly UPDATING database if a title/abstract is found within the archive file
+
+    """ given the correctly selected file, search for doi
+                :param file: list of raw xml files stored as json
+                :param id: id of doi within database for updating if title/abstract found
+                :param doi: to scan for right doi
+                :return: title/abstract, if both there or just title or nothing if not there
+                """
+
+    titles = []
+    abstracts = []
+    with open(file, "r") as f:
+        data = json.load(f)
+        print(data)
+        for key in data["entries"]:
+            for val in key.values():
+                if type(val) == str:
+                    doi_search = re.search("(10\.\d{4,9}\/[-._;()/:A-Za-z0-9]+)", val)
+                    if doi_search is not None and doi_search.group(0) == doi:
+                        if key["title"]:
+                            tag_match = re.compile(
+                                r'(<!--.*?-->|<[^>]*>)|(\[{1}\bASAP\]{1}\s*)|(\bMarine\s\bDrugs\,\ \bVol\.\s[0-9]*\,\s\bPages\s[0-9]*\:\s*)')
+                            clean_title = tag_match.sub('', key["title"])
+                            titles.append(clean_title)
+                        else:
+                            print("no title")
+                            # TODO: WORK ON ABSTRACT, not just summary, could be 'value' which is not in entries
+                        if re.search(
+                                '(^[A-Z][^.!?]*((\.|!|\?)(?! |\n|\r|\r\n)[^.!?]*)*(\.|!|\?)(?= |\n|\r|\r\n)\s?)(?:[A-Z][^.!?]*((\.|!|\?)(?! |\n|\r|\r\n)[^.!?]*)*(\.|!|\?)(?= |\n|\r|\r\n)\s)+',
+                                key["summary"]):
+                            abstract = key["summary"]
+                            abstracts.append(abstract)
+                        else:
+                            print("no abstract")
+    unique_titles = list(set(titles))
+    unique_abstracts = list(set(abstracts))
+    if unique_abstracts and unique_titles:
+        return unique_titles[0], unique_abstracts[0]
+    elif unique_titles:
+        return unique_titles[0]
+    else:
+        return ("no luck")
