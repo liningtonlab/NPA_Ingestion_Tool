970ad7ae4c35577f26b0331be58cf27cd806bef4
jvansan <jeffreyavansanten@gmail.com>
2021-03-09 10:31:30 -0800

    generalize refactored paths

--- main.py
+++ main.py
@@ -3,38 +3,48 @@ import npa_ingestion_tool
 from datetime import datetime
 import sqlite3
 from dotenv import load_dotenv
+
 load_dotenv()
-
-
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+DB_PATH = os.path.join(DIR_PATH, "npa_ingestion_database.db")
 WEBHOOK_URL = os.environ.get("SLACK_WEBHOOK_URL")
 
 
 def rss_feed_2_doi():
-    """ Open up file, loop through each line in the file to get each url to parse RSS feed and Append list of DOIs to
+    """Open up file, loop through each line in the file to get each url to parse RSS feed and Append list of DOIs to
     list for each journal. Saves a raw XML file for archive of RSS feed. Lastly, added new DOI's into SQLite3 database.
         :return: Count of added DOIs
-                        """
-    path_doi = 'C:/Users/maras/Desktop/NPA_Ingestion_Tool/rss_feeds/rss_feed_with_doi.txt'
+    """
+    path_doi = os.path.join(DIR_PATH, "rss_feeds/rss_feed_with_doi.txt")
     with open(path_doi, "r") as my_file:
-        '''doi_list = [npa_ingestion_tool.parse_rss(line, str(npa_ingestion_tool.no_newline(line) + "_" + datetime.now(
+        """doi_list = [npa_ingestion_tool.parse_rss(line, str(npa_ingestion_tool.no_newline(line) + "_" + datetime.now(
         ).strftime("%Y-%m-%d_%I-%M-%S_%p") + ".json")) for line in my_file]
-        '''
+        """
         doi_list = []
         for line in my_file:
-            string = npa_ingestion_tool.get_xml(line, str(
-                npa_ingestion_tool.no_newline(line) + "_" + datetime.now().strftime("%Y-%m-%d") + ".xml"))
+            string = npa_ingestion_tool.get_xml(
+                line,
+                str(
+                    npa_ingestion_tool.no_newline(line)
+                    + "_"
+                    + datetime.now().strftime("%Y-%m-%d")
+                    + ".xml"
+                ),
+            )
             parsed_doi_list = npa_ingestion_tool.parse_rss(string[0])
             doi_list.append((string[1], parsed_doi_list))
     print(doi_list)
 
     # SQLite3 Database entry
-    connection = npa_ingestion_tool.sqlite3_db_initialization("npa_ingestion_database.db")
+    connection = npa_ingestion_tool.sqlite3_db_initialization(DB_PATH)
     npa_ingestion_tool.sqlite3_table_creation(connection)
     doi_insert_count = 0
     for journal in doi_list:
         archive_filename = journal[0]
         for doi in journal[1]:
-            doi_insert_success = npa_ingestion_tool.sqlite3_insertion_only_doi(connection, doi, archive_filename)
+            doi_insert_success = npa_ingestion_tool.sqlite3_insertion_only_doi(
+                connection, doi, archive_filename
+            )
             if doi_insert_success:
                 doi_insert_count += 1
     # Database Viewing; In terminal type: sqlite3 + database name
@@ -44,29 +54,38 @@ def rss_feed_2_doi():
 
 
 def rss_feed_2_doi_elsevier():
-    """ Open up file, loop through each line in the file to get each url to parse RSS feed for article title to search
+    """Open up file, loop through each line in the file to get each url to parse RSS feed for article title to search
     CrossRef for DOI; Append list of DOIs to list for each journal. Saves a raw XML file for archive of RSS feed.
     Lastly, added new DOI's into SQLite3 database.
             :return: Count of added DOIs
-                        """
-    path_no_doi = 'C:/Users/maras/Desktop/NPA_Ingestion_Tool/rss_feeds/rss_feed_no_doi.txt'
+    """
+    path_no_doi = os.path.join(DIR_PATH, "rss_feeds/rss_feed_no_doi.txt")
     with open(path_no_doi, "r") as my_file:
         doi_lists = []
         for line in my_file:
-            string = npa_ingestion_tool.get_xml(line, str(
-                npa_ingestion_tool.no_newline(line) + "_" + datetime.now().strftime("%Y-%m-%d") + ".xml"))
+            string = npa_ingestion_tool.get_xml(
+                line,
+                str(
+                    npa_ingestion_tool.no_newline(line)
+                    + "_"
+                    + datetime.now().strftime("%Y-%m-%d")
+                    + ".xml"
+                ),
+            )
             parsed_doi_list = npa_ingestion_tool.parse_rss_no_doi(string[0])
             doi_lists.append((string[1], parsed_doi_list))
 
     print(doi_lists)
     # SQLite3 Database entry
-    connection = npa_ingestion_tool.sqlite3_db_initialization("npa_ingestion_database.db")
+    connection = npa_ingestion_tool.sqlite3_db_initialization(DB_PATH)
     npa_ingestion_tool.sqlite3_table_creation(connection)
     doi_insert_count_2 = 0
     for journal in doi_lists:
         archive_filename = journal[0]
         for doi in journal[1]:
-            doi_insert_success_2 = npa_ingestion_tool.sqlite3_insertion_only_doi(connection, doi, archive_filename)
+            doi_insert_success_2 = npa_ingestion_tool.sqlite3_insertion_only_doi(
+                connection, doi, archive_filename
+            )
             if doi_insert_success_2:
                 doi_insert_count_2 += 1
     connection.commit()
@@ -74,17 +93,18 @@ def rss_feed_2_doi_elsevier():
 
 
 def database_query_pubmed():
-    """ Connect to database and query DOIs for either less than or equal to or greater than 3 weeks. If less than 3
+    """Connect to database and query DOIs for either less than or equal to or greater than 3 weeks. If less than 3
     weeks, convert to pubmed id and search PubMed for title/abstract and updating the database if a result is found.
     If greater than equal to 3 weeks, try to parse from the RSS feed archives and also update database with any results.
-                            """
+    """
     # Database connection and cursor object creation
-    conn = npa_ingestion_tool.sqlite3_db_initialization("npa_ingestion_database.db")
+    conn = npa_ingestion_tool.sqlite3_db_initialization(DB_PATH)
     curse = conn.cursor()
 
     # Query PubMed (SELECT SQL statement) for DOI(title/abstract null) <3 weeks:
     new_rows = curse.execute(
-        "SELECT * FROM DOIs WHERE Created > date('now', '-21 day') and Abstract IS NULL and Title is NULL")
+        "SELECT * FROM DOIs WHERE Created > date('now', '-21 day') and Abstract IS NULL and Title is NULL"
+    )
     for doi in new_rows:
         print(doi)
 
@@ -92,21 +112,37 @@ def database_query_pubmed():
         pubmed_id = npa_ingestion_tool.doi_2_pmid(doi[1])  # returns type = list
         try:
             if pubmed_id:
-                pubmed_id_string = ''.join(pubmed_id)  # converts list (['123']) to string ('123')
+                pubmed_id_string = "".join(
+                    pubmed_id
+                )  # converts list (['123']) to string ('123')
                 title_abstract = npa_ingestion_tool.pmid_2_abstract(pubmed_id)
-                if title_abstract[1] is None or npa_ingestion_tool.blanks(title_abstract[1]) is True:
+                if (
+                    title_abstract[1] is None
+                    or npa_ingestion_tool.blanks(title_abstract[1]) is True
+                ):
                     try:
                         conn.execute(
                             "UPDATE DOIs SET PMID = ?, Title = ?, Source = ? WHERE ID = ?",
-                            (pubmed_id_string, title_abstract[0], "PubMed", doi[0]))
+                            (pubmed_id_string, title_abstract[0], "PubMed", doi[0]),
+                        )
                     except sqlite3.Error as error:
                         print("Failed to update sqlite table", error)
 
-                elif title_abstract[1] is not None or npa_ingestion_tool.blanks(title_abstract[1]) is False:
+                elif (
+                    title_abstract[1] is not None
+                    or npa_ingestion_tool.blanks(title_abstract[1]) is False
+                ):
                     try:
                         conn.execute(
                             "UPDATE DOIs SET PMID = ?, Title = ?, Abstract = ?, Source = ? WHERE ID = ?",
-                            (pubmed_id_string, title_abstract[0], title_abstract[1], "PubMed", doi[0]))
+                            (
+                                pubmed_id_string,
+                                title_abstract[0],
+                                title_abstract[1],
+                                "PubMed",
+                                doi[0],
+                            ),
+                        )
                     except sqlite3.Error as error:
                         print("Failed to update sqlite table", error)
             else:
@@ -120,7 +156,8 @@ def database_query_pubmed():
 
     # Query PubMed (SELECT SQL statement) for DOI(title/abstract null) >= 3 weeks:
     curse.execute(
-        "SELECT * FROM DOIs WHERE Abstract IS NULL and Created <= date('now', '-21 day')")
+        "SELECT * FROM DOIs WHERE Abstract IS NULL and Created <= date('now', '-21 day')"
+    )
 
     # Try to parse title/abstract from archived rss feed
     # Filename stored in database, use rss_parse_archive() with: file, doi with id for later
@@ -128,14 +165,18 @@ def database_query_pubmed():
 
 
 def database_stats_query():
-    """ Query database for stats to return.
-                :return: Count total DOI's, How many with titles and how many with both title/abstract
-                            """
-    conn = npa_ingestion_tool.sqlite3_db_initialization("npa_ingestion_database.db")
+    """Query database for stats to return.
+    :return: Count total DOI's, How many with titles and how many with both title/abstract
+    """
+    conn = npa_ingestion_tool.sqlite3_db_initialization(DB_PATH)
     curse = conn.cursor()
     total_doi_number = curse.execute("SELECT COUNT(*) FROM DOIs").fetchone()[0]
-    doi_with_title = curse.execute("SELECT COUNT(*) FROM DOIs WHERE Title is NOT NULL").fetchone()[0]
-    doi_with_abstract_title = curse.execute("SELECT COUNT(*) FROM DOIs WHERE Abstract is NOT NULL").fetchone()[0]
+    doi_with_title = curse.execute(
+        "SELECT COUNT(*) FROM DOIs WHERE Title is NOT NULL"
+    ).fetchone()[0]
+    doi_with_abstract_title = curse.execute(
+        "SELECT COUNT(*) FROM DOIs WHERE Abstract is NOT NULL"
+    ).fetchone()[0]
     return total_doi_number, doi_with_title, doi_with_abstract_title
 
 
@@ -145,20 +186,28 @@ if __name__ == "__main__":
     # DOI Insertion from Titles Parsed from RSS Feeds, Conversion to DOI via CrossRef. Returns count of inserted DOI's
     no_doi_feeds_insert_count = rss_feed_2_doi_elsevier()
     database_query_pubmed()  # Querying DOI's in DB on PubMed for title/abstract
-    database_stats = database_stats_query()  # Querying DB for counts of rows in DB with certain columns
+    database_stats = (
+        database_stats_query()
+    )  # Querying DB for counts of rows in DB with certain columns
     total_dois = database_stats[0]
     dois_only_title = database_stats[1]
     dois_title_abstract = database_stats[2]
 
-
     # Slack Stats Messaging
-    message = "Inserted DOI's parsed directly from RSS feeds: {0}\nInserted DOI's via CrossRef query of RSS feed " \
-              "parsed title: {1}\nTotal Database DOI's: {2}\n{3} with title only \n{4} with both title and " \
-              "abstract.".format(doi_feeds_insert_count,
-                                 no_doi_feeds_insert_count, total_dois, dois_only_title, dois_title_abstract)
+    message = (
+        "Inserted DOI's parsed directly from RSS feeds: {0}\nInserted DOI's via CrossRef query of RSS feed "
+        "parsed title: {1}\nTotal Database DOI's: {2}\n{3} with title only \n{4} with both title and "
+        "abstract.".format(
+            doi_feeds_insert_count,
+            no_doi_feeds_insert_count,
+            total_dois,
+            dois_only_title,
+            dois_title_abstract,
+        )
+    )
 
     # Change to True to stop notifications from being sent
-    notification_control_condition = False
+    notification_control_condition = True
 
     if notification_control_condition is False:
         npa_ingestion_tool.send_message(WEBHOOK_URL, message)
--- npa_ingestion_tool.py
+++ npa_ingestion_tool.py
@@ -19,31 +19,31 @@ from urllib.request import urlopen, Request
 import json
 from Levenshtein import ratio
 
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
 
 def send_message(webhook_url, message):
     requests.post(webhook_url, json={"text": message})
 
 
 def cross_ref_query_title(title):
-    """ Open up file, loop through each line in the file to get each url to parse RSS feed for article title to search
+    """Open up file, loop through each line in the file to get each url to parse RSS feed for article title to search
     CrossRef for DOI; Append list of DOIs to list for each journal. Saves a raw XML file for archive of RSS feed. Lastly
     , added new DOI's into SQLite3 database.
                 :param: string of title
                 :return: True/False for success; if success has title, DOI and similarity match. Otherwise,
                 returns exception.
-                            """
-    empty_result = {
-        "crossref_title": "",
-        "similarity": 0,
-        "doi": ""
-    }
+    """
+    empty_result = {"crossref_title": "", "similarity": 0, "doi": ""}
     api_url = "https://api.crossref.org/works?"
     params = {"rows": "5", "query.bibliographic": title}
     url = api_url + urlencode(params, quote_via=quote_plus)
     print(url)
     request = Request(url)
-    request.add_header("User-Agent",
-                       "NPA Ingestion Tool (https://github.com/liningtonlab/NPA_Ingestion_Tool; mailto:andrew_maras@sfu.ca)")
+    request.add_header(
+        "User-Agent",
+        "NPA Ingestion Tool (https://github.com/liningtonlab/NPA_Ingestion_Tool; mailto:andrew_maras@sfu.ca)",
+    )
     try:
         ret = urlopen(request)
         content = ret.read()
@@ -56,13 +56,19 @@ def cross_ref_query_title(title):
             title = item["title"].pop()
             result = {
                 "crossref_title": title,
-                "similarity": ratio(title.lower(), params["query.bibliographic"].lower()),
-                "doi": item["DOI"]
+                "similarity": ratio(
+                    title.lower(), params["query.bibliographic"].lower()
+                ),
+                "doi": item["DOI"],
             }
             if most_similar["similarity"] < result["similarity"]:
                 most_similar = result
         if most_similar["similarity"] < 0.75:
-            return {"success": False, "result": most_similar, "exception": "Low Similarity"}
+            return {
+                "success": False,
+                "result": most_similar,
+                "exception": "Low Similarity",
+            }
         else:
             return {"success": True, "result": most_similar}
     except HTTPError as httpe:
@@ -70,23 +76,35 @@ def cross_ref_query_title(title):
 
 
 def no_newline(line):
-    """ Takes string input and checks if blank or not
-                    :param line: string
-                    :return: returns boolean of true for a blank, false if not blank
-                    """
-    new_line = line.rstrip('\n')
-    replace_line = new_line.replace("/", "").replace(".", "").replace(":", "").replace("-", "").replace(
-        "=", "").replace(
-        "!", "").replace("@", "").replace("#", "").replace(
-        "$", "").replace("%", "").replace("^", "").replace("&", "").replace("*", "").replace("?", "")
+    """Takes string input and checks if blank or not
+    :param line: string
+    :return: returns boolean of true for a blank, false if not blank
+    """
+    new_line = line.rstrip("\n")
+    replace_line = (
+        new_line.replace("/", "")
+        .replace(".", "")
+        .replace(":", "")
+        .replace("-", "")
+        .replace("=", "")
+        .replace("!", "")
+        .replace("@", "")
+        .replace("#", "")
+        .replace("$", "")
+        .replace("%", "")
+        .replace("^", "")
+        .replace("&", "")
+        .replace("*", "")
+        .replace("?", "")
+    )
     return replace_line
 
 
 def blanks(string):
-    """ Takes string input and checks if blank or not
-                :param string: string
-                :return: returns boolean of true for a blank, false if not blank
-                """
+    """Takes string input and checks if blank or not
+    :param string: string
+    :return: returns boolean of true for a blank, false if not blank
+    """
     if string == "":
         return True
     else:
@@ -94,47 +112,47 @@ def blanks(string):
 
 
 def get_xml(url, archive_filename):
-    """ parse a xml file retrieved with requests using feedparser to the gather list of DOIs from the
-                    specified xml
-                :param url: url of rss feed
-                :param archive_filename: The file name of the archive file
-                :return: raw string of text from requests.get(url)
-                """
+    """parse a xml file retrieved with requests using feedparser to the gather list of DOIs from the
+        specified xml
+    :param url: url of rss feed
+    :param archive_filename: The file name of the archive file
+    :return: raw string of text from requests.get(url)
+    """
     # Assign a user agent to avoid 403 error
-    user_agent = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    user_agent = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
 
     # Using request to get RSS feed text from the URL using the assigned user agent as headers variable
-    no_whitespace_url = url.rstrip('\n')
+    no_whitespace_url = url.rstrip("\n")
 
-    path = 'C:/Users/maras/Desktop/NPA_Ingestion_Tool/raw_xml_archive/' + archive_filename
+    path = os.path.join(DIR_PATH, "raw_xml_archive", archive_filename)
 
     if not os.path.exists(path):
         url_request = requests.get(no_whitespace_url, headers=user_agent)
         if not url_request.status_code == 200:
             print(f"WARNING: {url_request.status_code} for URL = {url}")
         raw_xml_str = url_request.text
-        with open(path, "w", encoding='utf-8') as f:
+        with open(path, "w", encoding="utf-8") as f:
             f.write(raw_xml_str)
         return raw_xml_str, archive_filename
     else:
-        with open(path, "r", encoding='utf-8') as f:
+        with open(path, "r", encoding="utf-8") as f:
             xml = f.read()
         return xml, archive_filename
 
 
 def parse_rss(string_input):
-    """ parse a xml file retrieved with requests using feedparser to the gather list of DOIs from the
-                specified xml
-            :param string_input: raw string of text from requests.get(url)
-            :return: list of unique DOIs
-            """
+    """parse a xml file retrieved with requests using feedparser to the gather list of DOIs from the
+        specified xml
+    :param string_input: raw string of text from requests.get(url)
+    :return: list of unique DOIs
+    """
     # Parse RSS url w/ feedparser to get consistent format
     rss_feed = feedparser.parse(string_input)
 
     # Creation of JSON file for easy viewing (For archive of FeedparserDict using JSON, outdated)
-    '''path = 'C:/Users/maras/Desktop/NPA_Ingestion_Tool/raw_xml_archive/' + archive_file
+    """path = 'C:/Users/maras/Desktop/NPA_Ingestion_Tool/raw_xml_archive/' + archive_file
     with open(path, "x") as f:
-        json.dump(rss_feed, f)'''
+        json.dump(rss_feed, f)"""
 
     # New list for found DOIs (will end up with duplicates)
     doi_list = []
@@ -156,18 +174,18 @@ def parse_rss(string_input):
 
 
 def parse_rss_no_doi(string_input):
-    """ parse a xml file retrieved with requests using feedparser to the gather list of DOIs from the
-                specified xml, by searching RSS feeds for titles to query on CrossRef for DOIs.
-            :param string_input: raw string of text from requests.get(url)
-            :return: list of unique DOIs
-            """
+    """parse a xml file retrieved with requests using feedparser to the gather list of DOIs from the
+        specified xml, by searching RSS feeds for titles to query on CrossRef for DOIs.
+    :param string_input: raw string of text from requests.get(url)
+    :return: list of unique DOIs
+    """
     # Parse RSS url w/ feedparser to get consistent format
     rss_feed = feedparser.parse(string_input)
 
     # Creation of JSON file for easy viewing (For archive of FeedparserDict using JSON, outdated)
-    '''path = 'C:/Users/maras/Desktop/NPA_Ingestion_Tool/raw_xml_archive/' + archive_file
+    """path = 'C:/Users/maras/Desktop/NPA_Ingestion_Tool/raw_xml_archive/' + archive_file
     with open(path, "x") as f:
-        json.dump(rss_feed, f)'''
+        json.dump(rss_feed, f)"""
 
     # New list for found DOIs (will end up with duplicates)
     crossref_doi_list = []
@@ -179,13 +197,19 @@ def parse_rss_no_doi(string_input):
             title_1 = key["title"]
             found_title.append(title_1)
         unique_title = list(set(found_title))
-        title_string = ''.join(unique_title)
+        title_string = "".join(unique_title)
         crossref_query = cross_ref_query_title(title_string)
         found_doi = crossref_query["result"]["doi"]
         if crossref_query["success"]:
             crossref_doi_list.append(found_doi)
         elif not crossref_query["success"]:
-            print((key["title"], crossref_query["result"]["crossref_title"], crossref_query["exception"]))
+            print(
+                (
+                    key["title"],
+                    crossref_query["result"]["crossref_title"],
+                    crossref_query["exception"],
+                )
+            )
             continue
 
     # Remove duplicate DOIs from list
@@ -195,20 +219,20 @@ def parse_rss_no_doi(string_input):
 
 
 def doi_2_pmid(doi):
-    """ returns PMID for a given DOI
-                    :param doi:  DOI as string
-                    :return: PMID as string
-                    """
+    """returns PMID for a given DOI
+    :param doi:  DOI as string
+    :return: PMID as string
+    """
     fetch = PubMedFetcher()
     pm_id = fetch.pmids_for_query(f"{doi}[DOI]")
     return pm_id
 
 
 def pmid_2_abstract(pm_id):
-    """ returns abstract for a given pmid
-                :param pm_id: string pmid
-                :return: tuple of title and abstract
-                """
+    """returns abstract for a given pmid
+    :param pm_id: string pmid
+    :return: tuple of title and abstract
+    """
     fetch = PubMedFetcher()
     article = fetch.article_by_pmid(pm_id)
     article_xml = article.xml
@@ -225,40 +249,42 @@ def pmid_2_abstract(pm_id):
 
 
 def sqlite3_db_initialization(db_file):
-    """ create a database connection to the SQLite database
-            specified by db_file
-        :param db_file: database file
-        :return: Connection object or None
-        """
+    """create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
     db_connection = sqlite3.connect(db_file)
     return db_connection
 
 
 def sqlite3_table_creation(connection_object):
-    """ create a table from the create_table_sql statement
-        :param connection_object: Connection object
-        :return: Cursor object or None
-        """
+    """create a table from the create_table_sql statement
+    :param connection_object: Connection object
+    :return: Cursor object or None
+    """
     cursor = connection_object.cursor()
     cursor.execute(
-        "CREATE TABLE IF NOT EXISTS DOIs (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,Doi VARCHAR UNIQUE NOT NULL, PMID VARCHAR, Title VARCHAR, Abstract VARCHAR, Source VARCHAR, Filename NOT NULL, Created NOT NULL)")
+        "CREATE TABLE IF NOT EXISTS DOIs (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,Doi VARCHAR UNIQUE NOT NULL, PMID VARCHAR, Title VARCHAR, Abstract VARCHAR, Source VARCHAR, Filename NOT NULL, Created NOT NULL)"
+    )
     return cursor
 
 
 def sqlite3_insertion_only_doi(connection_object, doi, filename):
-    """ insert data into the SQLite3 database
-            :param filename: name of archive file
-            :param doi: Doi
-            :param connection_object: Connection object.cursor()
-            :return: None
-            """
+    """insert data into the SQLite3 database
+    :param filename: name of archive file
+    :param doi: Doi
+    :param connection_object: Connection object.cursor()
+    :return: None
+    """
     # Data insertion into the database
 
     try:
         with connection_object:
             connection_object.execute(
                 "INSERT INTO DOIs(Doi, PMID, Title, Abstract, Source, Filename, Created) VALUES(?, NULL, NULL, NULL, NULL, ?, ?)",
-                (doi, filename, datetime.now()))
+                (doi, filename, datetime.now()),
+            )
             print("Successful DOI entry recorded!")
             return True
     except sqlite3.IntegrityError:
